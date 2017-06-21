0ec7af8f89265b3a8a98110ec8f3e30b89282829
Jason Stallings <jason@stallin.gs>
2017-06-21 09:33:06 -0500

    Fix

--- git_push.sh
+++ git_push.sh
@@ -13,7 +13,7 @@ if [ "$git_user_id" = "" ]; then
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="wp-unit-test-api"
+    git_repo_id="WP-Unit-Test-API-Client-PHP"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
